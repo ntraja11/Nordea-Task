77044dd430d1b6c059e050a788fa721bbe60529d
Raja Anand Narayanan <ntraja11@gmail.com>
2016-05-22 14:52:17 +0530

    After Venue Loading success

--- js/searchScript.js
+++ js/searchScript.js
@@ -4,7 +4,8 @@
             requires = [],
             searchApp = null,
             locationData,
-            url = "https://api.foursquare.com/v2/venues/search?client_id=xxxx&client_secret=zzzz&v=20160521",            
+            latlong,
+            url = "https://api.foursquare.com/v2/venues/search?client_id=xxxx&client_secret=zzzz&v=20160522&m=foursquare&oauth_token=C5FTPYSYZX0G3M00TKQJCOP3CWBBIQP02YHBQKHIJ0JLD4RP",            
             config
             ;
             
@@ -13,10 +14,7 @@
             searchApp.factory("searchSVC", function($http, $q){
                   
               return {                  
-                  getVenues: function(){                                            
-                        var latlong = locationData.coords.latitude + ',' + locationData.coords.longitude;
-                        console.log("loading venues from serice :: " + latlong);
-                        config = {params:{ll:latlong}};
+                  getVenues: function(){                                                                    
                         var deferred = $q.defer();
 
                         $http.get(url, config)
@@ -31,37 +29,56 @@
                 }
             });
 
-            searchApp.controller("SearchCtrl", function($scope, $http, searchSVC){            
+            searchApp.controller("SearchCtrl", function($scope, $http, searchSVC){
 
-                  $scope.loadVenues = function(){
+                  $scope.venueList = [];         
+                  $scope.searchKey = "Public";
+
+                  $scope.loadVenues = function(){                        
+                        
                         searchSVC.getVenues().then(success, error);
 
-                        function success(data){
-                          console.log("Venue Loading Success :: " + data);
-                          //$scope.contacts = data;
-                          //$scope.toggleContactTable = true;
+                        function success(data){                          
+                          $scope.venueList = data.response.venues;   
+                          console.log("Venue Loading Success :: ");
+                          for(venue in $scope.venueList){
+                              console.log("Venue Name :: " + $scope.venueList[venue].name + " Venue Distance :: " + $scope.venueList[venue].location.distance);
+                          }           
                         }
                         function error(er){
                           console.log("Venue Loading error : ", er);
                         }                 
                     }
+
+                  $scope.setConfig = function(){
+                        
+                        if($scope.searchKey == ""){                              
+                              config = {params:{ll:latlong}};
+                        }else{
+                              config = {params:{ll:latlong, intent:"match", query:$scope.searchKey}};      
+                        }
+                        $scope.loadVenues();                        
+                  }
+
+                  $scope.getSearchKey = function(){
+                        console.log("Search Key is :: " + $scope.searchKey);
+                  }
                   
                   
 
                   $scope.getLocation = function() {
                       if (navigator.geolocation) {
-                          navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
+                          navigator.geolocation.getCurrentPosition($scope.setPosition, $scope.showError);                          
                       } else { 
                           console.log("Geolocation is not supported by this browser.");
                       }
                   }
 
-                  $scope.showPosition = function(position) {                        
+                  $scope.setPosition = function(position) {                        
                         locationData = position;
-                        $scope.loadVenues();
-                        //console.log("Getting location from NG :: ");
-                        //console.log("Latitude :: " + locationData.coords.latitude);
-                        //console.log("Longitude :: " + locationData.coords.longitude);     
+                        latlong = locationData.coords.latitude + ',' + locationData.coords.longitude;
+                        //console.log("loading venues from service :: " + latlong);
+                        $scope.setConfig();                        
                   }
 
                   $scope.showError = function(error) {
