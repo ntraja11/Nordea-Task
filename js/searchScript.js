a93bc51873d348882b82ecd9ede560382589b7ca
Raja Anand Narayanan <ntraja11@gmail.com>
2016-08-18 02:33:28 +0530

    Commit after responsive coding

--- js/searchScript.js
+++ js/searchScript.js
@@ -6,17 +6,20 @@
             searchApp = null,
             locationData,
             latlong,
-            url = "https://api.foursquare.com/v2/venues/search?client_id=xxxx&client_secret=zzzz&v=20160522&m=foursquare&oauth_token=C5FTPYSYZX0G3M00TKQJCOP3CWBBIQP02YHBQKHIJ0JLD4RP",            
-            config
+            authToken = "C5FTPYSYZX0G3M00TKQJCOP3CWBBIQP02YHBQKHIJ0JLD4RP",
+            baseUrl = "https://api.foursquare.com/v2/venues/",
+            searchUrl = baseUrl + "search?client_id=xxxx&client_secret=zzzz&v=20160807&m=foursquare&oauth_token=" + authToken,            
+            config,
+            photoUrl = "/photos?v=20160807&oauth_token=" + authToken
             ;
-            
+
             searchApp = angular.module(name, requires);
 
             searchApp.factory("searchSVC", function($http, $q){                  
               return {                  
                   getVenues: function(){                                                                    
                         var deferred = $q.defer();
-                        $http.get(url, config)
+                        $http.get(searchUrl, config)
                             .success(function(data){                        
                               deferred.resolve(data);
                             })
@@ -24,7 +27,18 @@
                               deferred.reject(error);
                             });
                             return deferred.promise;
-                  }                         
+                  },
+                  getPhoto:function(venueId){
+                      var deferred = $q.defer();
+                        $http.get(baseUrl + venueId + photoUrl)
+                            .success(function(data){                        
+                              deferred.resolve(data);
+                            })
+                            .error(function(error){                        
+                              deferred.reject(error);
+                            });
+                            return deferred.promise; 
+                  }
                 };
             });
 
@@ -32,16 +46,38 @@
 
                   $scope.venueList = [];         
                   $scope.searchKey = "";
-                  $scope.venueListLimit;
+                  $scope.venueListLimit = 2;
+                  $scope.photoObj = [];
 
                   $scope.loadVenues = function(){                         
                         searchSVC.getVenues().then(function success(data){
-                              $scope.venueList = data.response.venues;  
-                              $scope.venueListLimit =  $scope.venueList.length;                                
+                              $scope.venueList = data.response.venues;                                
+                              for (var i = 0; i < $scope.venueList.length; i++) {
+                                  $scope.getVenueImagePath($scope.venueList[i], i);
+                                  //console.dir($scope.venueList[i]);
+                              }                              
                         }, function error(er){
                           console.log("Venue Loading error : ", er);    
                         });
                     };
+
+                  $scope.getVenueImagePath = function(venueObj, cnt){
+                      searchSVC.getPhoto(venueObj.id).then(function success(data){
+                          //venueObj.imageUrl = "noImage.jpeg";
+                          venueObj.imageUrl = "";
+                          if(data.response.photos.items.length){
+                            venueObj.imageUrl = data.response.photos.items[0].prefix + 
+                              data.response.photos.items[0].width + "x" + data.response.photos.items[0].height +
+                              data.response.photos.items[0].suffix;  
+                              $scope.photoObj.push(venueObj);
+                              $scope.venueListLimit =  $scope.photoObj.length; 
+                              console.log("Location with Photo : " + $scope.photoObj.length);
+                          }
+                          //console.log("Photo for venue : " + cnt + " : " + venueObj.imageUrl);
+                        }, function error(err){
+                              console.log("Photo Loading error : ", err);
+                      });
+                  }
 
                   $scope.setConfig = function(){                        
                         if($scope.searchKey === ""){                              
